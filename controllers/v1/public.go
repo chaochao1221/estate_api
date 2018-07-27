39de682978bcaab3031698b6e99a5b84692cad78
dingchao <934234902@qq.com>
2018-07-27 17:30:27 +0800

    fix

--- controllers/v1/public.go
+++ controllers/v1/public.go
@@ -16,12 +16,14 @@ func Public(parentRoute *gin.RouterGroup) {
 	router.Use(middleware.Auth())
 	router.GET("/company_detail", Public_CompanyDetail)          // 3.1 公用-公司详情
 	router.GET("/sales_manage/list", Public_SalesManageList)     // 3.2.1 公用-销售管理-列表
-	router.GET("/sales_manage/detail", Public_SalesManageDetail) // 3.2.1 公用-销售管理-详情
+	router.GET("/sales_manage/detail", Public_SalesManageDetail) // 3.2.2 公用-销售管理-详情
+	router.POST("/sales_manage/add", Public_SalesManageAdd)      // 3.2.3 公用-销售管理-添加/编辑
+	router.DELETE("/sales_manage/del", Public_SalesManageDel)    // 3.2.4 公用-销售管理-删除
 }
 
 // 公用-公司详情
 func Public_CompanyDetail(c *gin.Context) {
-	userId, _ := strconv.Atoi(c.Query("user_id"))
+	userId, _ := strconv.Atoi(c.Request.Header.Get("user_id"))
 	if userId == 0 {
 		c.JSON(400, gin.H{
 			"code": 1010,
@@ -49,9 +51,9 @@ func Public_CompanyDetail(c *gin.Context) {
 
 // 公用-销售管理列表
 func Public_SalesManageList(c *gin.Context) {
-	userId, _ := strconv.Atoi(c.Query("user_id"))
-	userType, _ := strconv.Atoi(c.Query("user_type"))
-	if userId == 0 || userType == 0 {
+	userId, _ := strconv.Atoi(c.Request.Header.Get("user_id"))
+	userType, _ := strconv.Atoi(c.Request.Header.Get("user_type"))
+	if userId == 0 {
 		c.JSON(400, gin.H{
 			"code": 1010,
 			"msg":  "参数错误",
@@ -111,6 +113,82 @@ func Public_SalesManageDetail(c *gin.Context) {
 		"code": 0,
 		"msg":  "success",
 		"data": data,
+	})
+	return
+}
+
+// 公用-销售管理添加/编辑
+func Public_SalesManageAdd(c *gin.Context) {
+	leaderUserId, _ := strconv.Atoi(c.Request.Header.Get("user_id"))
+	userType, _ := strconv.Atoi(c.Request.Header.Get("user_type"))
+	userId, _ := strconv.Atoi(c.PostForm("user_id")) // 添加非必传，编辑必传
+	name := c.PostForm("name")
+	email := c.PostForm("email")
+	password := c.PostForm("password") // 添加必传，编辑非必传
+	if leaderUserId == 0 || name == "" || email == "" {
+		c.JSON(400, gin.H{
+			"code": 1010,
+			"msg":  "参数错误",
+		})
+		return
+	}
+	if userType == 0 {
+		c.JSON(400, gin.H{
+			"code": 1010,
+			"msg":  "该账户非公司主管，不允许添加/编辑员工",
+		})
+		return
+	}
+
+	// 添加/编辑
+	errMsg := publicModel.Public_SalesManageAdd(leaderUserId, userId, name, email, password)
+	if errMsg != "" {
+		c.JSON(400, gin.H{
+			"code": 1010,
+			"msg":  errMsg,
+		})
+		return
+	}
+	c.JSON(201, gin.H{
+		"code": 0,
+		"msg":  "success",
+	})
+	return
+}
+
+// 公用-销售管理删除
+func Public_SalesManageDel(c *gin.Context) {
+	leaderUserId, _ := strconv.Atoi(c.Request.Header.Get("user_id"))
+	groupId, _ := strconv.Atoi(c.Request.Header.Get("group_id"))
+	userType, _ := strconv.Atoi(c.Request.Header.Get("user_type"))
+	userId, _ := strconv.Atoi(c.PostForm("user_id")) // 添加非必传，编辑必传
+	if groupId == 0 || userId == 0 {
+		c.JSON(400, gin.H{
+			"code": 1010,
+			"msg":  "参数错误",
+		})
+		return
+	}
+	if userType == 0 {
+		c.JSON(400, gin.H{
+			"code": 1010,
+			"msg":  "该账户非公司主管，不允许删除员工",
+		})
+		return
+	}
+
+	// 删除
+	errMsg := publicModel.Public_SalesManageDel(leaderUserId, groupId, userId)
+	if errMsg != "" {
+		c.JSON(400, gin.H{
+			"code": 1010,
+			"msg":  errMsg,
+		})
+		return
+	}
+	c.JSON(201, gin.H{
+		"code": 0,
+		"msg":  "success",
 	})
 	return
 }
--- models/v1/public.go
+++ models/v1/public.go
@@ -117,3 +117,98 @@ func (this *PublicModel) Public_SalesManageDetail(userId int) (data *PublicSales
 		Email:  userInfo.Email,
 	}, ""
 }
+
+// 公用-销售管理添加/编辑
+func (this *PublicModel) Public_SalesManageAdd(leaderUserId, userId int, name, email, password string) (errMsg string) {
+	// 用户信息
+	userInfo, errMsg := userModel.GetUserInfo(&GetUserInfoParameter{UserId: leaderUserId})
+	if errMsg != "" {
+		return errMsg
+	}
+	if userInfo == nil {
+		return "该用户不存咋"
+	}
+
+	// 根据userId是否为0来判断是添加还是编辑
+	if userId == 0 { // 添加
+		sql := `INSERT INTO p_user(company_id, email, password, name) VALUES(?,?,?,?)`
+		_, err := db.Db.Exec(sql, userInfo.CompanyId, email, password, name)
+		if err != nil {
+			return "添加用户失败"
+		}
+	} else { // 编辑
+		sql := `UPDATE p_user
+				SET email=?, password=?, name=?
+				WHERE id=?`
+		_, err := db.Db.Exec(sql, email, password, name, userId)
+		if err != nil {
+			return "编辑用户失败"
+		}
+	}
+
+	return
+}
+
+// 公用-销售管理删除
+func (this *PublicModel) Public_SalesManageDel(leaderUserId, groupId, userId int) (errMsg string) {
+	// 开启事务
+	transaction := db.Db.NewSession()
+	if err := transaction.Begin(); err != nil {
+		return "开启事务失败"
+	}
+
+	// 中介删除（本部、中国、日本）
+	// 1）中介用户身份物理删除
+	// 1）中国中介、日本中介相关数据挂到该公司主管上
+	// 2）本部中介房源信息挂到本部主管上，本部中介客户信息进入待分配状态。销售业绩、中介费用统计中没有中介关系的数据归入其他
+	sql := `DELETE FROM p_user WHERE id=?`
+	_, err := transaction.Exec(sql, userId)
+	if err != nil {
+		transaction.Rollback()
+		return "删除用户身份失败"
+	}
+
+	switch groupId {
+	case 1: // 本部
+		// 更改房源发布者
+		sql = `UPDATE p_estate SET user_id=? WHERE user_id=?`
+		_, err = transaction.Exec(sql, leaderUserId, userId)
+		if err != nil {
+			transaction.Rollback()
+			return "更新本部发布者失败"
+		}
+
+		// 删除分配关系
+		sql = `DELETE FROM base_distribution WHERE user_id=?`
+		_, err = transaction.Exec(sql, userId)
+		if err != nil {
+			transaction.Rollback()
+			return "删除分配状态失败"
+		}
+	case 2: // 中国
+		// 更改推荐者
+		sql = `UPDATE p_recommend SET user_id=? WHERE user_id=?`
+		_, err = transaction.Exec(sql, leaderUserId, userId)
+		if err != nil {
+			transaction.Rollback()
+			return "更新推荐者失败"
+		}
+	case 3: // 日本
+		// 更改房源发布者
+		sql = `UPDATE p_estate SET user_id=? WHERE user_id=?`
+		_, err = transaction.Exec(sql, leaderUserId, userId)
+		if err != nil {
+			transaction.Rollback()
+			return "更新日本中介发布角色失败"
+		}
+	default:
+		return "不存在该中介分组"
+	}
+
+	// 提交事务
+	if err := transaction.Commit(); err != nil {
+		transaction.Rollback()
+		return "提交事务失败"
+	}
+	return
+}
